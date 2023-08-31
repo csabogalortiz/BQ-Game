5626f7d46557c58f0a88c65ff19ec93d549ab880
Catalina Sabogal <catalina.sabogalo@utadeo.edu.co>
2023-08-31 12:31:32 -0600

    collected coordinates and IDs appear next to each other

--- src/scenes/FarmUI.ts
+++ src/scenes/FarmUI.ts
@@ -5,7 +5,8 @@ export default class FarmUI {
     private scene: Phaser.Scene;
     private coordinatesLabel?: Phaser.GameObjects.Text;
     private idLabel?: Phaser.GameObjects.Text;
-    private collectedImages: Phaser.GameObjects.Image[] = [];
+    private collectedCoordinatesImages: Phaser.GameObjects.Image[] = [];
+    private collectedIdImages: Phaser.GameObjects.Image[] = [];
 
     constructor(scene: Phaser.Scene, fontStyles) {
         this.scene = scene;
@@ -27,21 +28,30 @@ export default class FarmUI {
     private handleIdCollected() {
         this.createCollectedImage('id');
     }
-
     private createCollectedImage(imageKey: string) {
-        const x = 730; // X position for both coordinates and IDs
-        let y = 120 + this.collectedImages.length * 30; // Y position based on the number of collected images
-
-        const collectedImage = this.scene.add.image(x, y, imageKey);
-        collectedImage.setScale(0.5);
-        collectedImage.setVisible(true);
-        collectedImage.setScrollFactor(0, 0);
-
-        this.collectedImages.push(collectedImage);
+        let x = 730; // X position for both coordinates and IDs
+        let y = 0; // Y position is initialized
+        
+        const label = imageKey === 'coordinates' ? this.coordinatesLabel : this.idLabel;
+    
+        if (label) {
+            if (imageKey === 'coordinates') {
+                x += this.collectedCoordinatesImages.length * 40; // Adjust spacing as needed
+            } else {
+                x += this.collectedIdImages.length * 40; // Adjust spacing as needed
+            }
+    
+            const collectedImage = this.scene.add.image(x, label.y + label.displayHeight + 10, imageKey);
+            collectedImage.setScale(0.9); // Adjust the scale as needed
+            collectedImage.setVisible(true);
+            collectedImage.setScrollFactor(0, 0);
+    
+            if (imageKey === 'coordinates') {
+                this.collectedCoordinatesImages.push(collectedImage);
+            } else {
+                this.collectedIdImages.push(collectedImage);
+            }
+        }
     }
-}
-
-
-
-
+    }
 
